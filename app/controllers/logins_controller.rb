03ead914e987b1e564055a7a5ad3721688411204
Ben <fengzibin0828@gmail.com>
2015-04-10 17:55:30 +0800

    update

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,37 +1,43 @@
 class LoginsController < ApplicationController
-	def login
-		unless request.get?
-			@hint = "ok"
-			@id = params[:basic][:idenity]
-			@emp_num = params[:basic][:emp_number]
+  def login
+    unless request.get?
+      @hint = "ok"
+        @id = params[:basic][:idenity]
+        @emp_num = params[:basic][:emp_number]
 
-			if @id == '1'
-				@user = Basic.where("emp_number = ?", params[:basic][:emp_number]).first
-			else
-				@user = Admin.where("username = ?", params[:basic][:emp_number]).first
-			end
+        if @id == '1'
+          @user = Basic.where("emp_number = ?", params[:basic][:emp_number]).first
+          else
+          @user = Admin.where("username = ?", params[:basic][:emp_number]).first
+        end
 
-			if @user == nil
-				@hint = '用户不存在！'
-				render action: 'login'
-			elsif @user.password == params[:basic][:password]
-				cookies[:emp_id] = @id
-				cookies[:emp_num] = @emp_num
-				if @id == '1'
-					render '/users/wait.html.erb'
-				elsif @id == '2'
-					render '/managements/wait.html.erb'
-				end
-			else
-				@hint = "登录失败，请检查您的登录信息是否有误"
-			end
-		end
-	end
+        if @user == nil
+          @hint = '用户不存在！'
+          render action: 'login'
+        elsif @user.password == params[:basic][:password]
+          cookies[:emp_id] = @id
+          cookies[:emp_num] = @emp_num
+          if @id == '1'
+            render '/users/wait.html.erb'
+          elsif @id == '2'
+            render '/managements/wait.html.erb'
+          end
+        else
+          @hint = "登录失败，请检查您的登录信息是否有误"
+        end
+    end
 
-	private
+    if request.url == root_path
+      cookies.delete(:emp_num)
+      cookies.delete(:emp_id)
+    end
+  end
 
-		def basic_params
-			params.require(:basic).permit(:emp_number, :password)
-		end
+  private
+
+  def basic_params
+    params.require(:basic).permit(:emp_number, :password)
+  end
 end
+
 
--- app/controllers/managements_controller.rb
+++ app/controllers/managements_controller.rb
@@ -1,8 +1,14 @@
 class ManagementsController < ApplicationController
-	def index
-		
-	end
+  def index
+    @num = cookies[:emp_num]
+    @id = cookies[:emp_id]
+    if @num == '' || @id == ''
+      @hint = "您还没登录，请您先登录"
+      cookies[:hint] = @hint
+      redirect_to root_path
+    end
+  end
 
-	def create
-	end
+  def create
+  end
 end

ed7c06f32d8bc8ffeb7760050e1518e9db1d595c
Ben <fengzibin0828@gmail.com>
2015-04-14 09:20:00 +0800

    2015-4-14 9:15

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
   def index
+    @num = cookies[:emp_num]
+    @id = cookies[:emp_id]
+    if @num == '' || @id == ''
+      @hint = "您还没登录，请您先登录"
+      cookies[:hint] = @hint
+      redirect_to root_path
+    end
   end
 end
